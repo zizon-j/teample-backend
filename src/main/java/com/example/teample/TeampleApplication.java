9945d2528965712d406f8c262077c6303b763f6c
tjwjddn <tjwjddn1025@naver.com>
2025-04-03 15:44:34 +0900

    Project Start

---
+++ src/main/java/com/example/teample/TeampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.teample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teample/TeampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
