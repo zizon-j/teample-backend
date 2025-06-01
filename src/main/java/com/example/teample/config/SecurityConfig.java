91cba7cfac8b88a67dcae61f4c04422c9a5bad1a
tjwjddn <tjwjddn1025@naver.com>
2025-06-01 18:27:12 +0900

    feat. WebConfig 허용범위 설정, CustomUserDetailsService 로그인 요청 로그 추가

--- src/main/java/com/example/teample/config/SecurityConfig.java
+++ src/main/java/com/example/teample/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/users/**").permitAll()
                         .requestMatchers("/api/auth/login").permitAll()
                         // 그 외의 모든 요청은 인증 필요
-                        .anyRequest().authenticated()
+                        .anyRequest().authenticated() // 나중에 지워야한다면 지워도 됨
                 ).sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 );
--- src/main/java/com/example/teample/user/security/CustomUserDetailsService.java
+++ src/main/java/com/example/teample/user/security/CustomUserDetailsService.java
@@ -4,12 +4,14 @@ package com.example.teample.user.security;
 import com.example.teample.user.domain.User;
 import com.example.teample.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -17,6 +19,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("로그인 요청 email: {}", email);
         //로그인 시 전달받은 username(email)을 이용해 DB에서 유저 정보를 조회합니다.
         User user = userRepository.findByUserEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("이메일을 찾을 수 없습니다." + email));
